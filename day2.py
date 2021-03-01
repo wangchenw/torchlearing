2d06e6eea528e10cd46441260d129abd511b1121
however <474141956@qq.com>
2021-03-01 12:22:50 +0800

    day2

--- day2.py
+++ day2.py
@@ -91,3 +91,28 @@ b.requires_grad_(requires_grad=True)
 #%%线性回归的矢量计算表达式的实现。我们使用mm函数做矩阵乘法
 def linreg(X, w, b):  # 本函数已保存在d2lzh_pytorch包中方便以后使用
     return torch.mm(X, w) + b
+#%%定义损失函数
+def squared_loss(y_hat, y):  # 本函数已保存在d2lzh_pytorch包中方便以后使用
+    # 注意这里返回的是向量, 另外, pytorch里的MSELoss并没有除以 2
+    return (y_hat - y.view(y_hat.size())) ** 2 / 2
+#%% 定义优化算法 小批量随机梯度下降算法  它通过不断迭代模型参数来优化损失函数
+def sgd(params, lr, batch_size):  # 本函数已保存在d2lzh_pytorch包中方便以后使用
+    for param in params:
+        param.data -= lr * param.grad / batch_size # 注意这里更改param时用的param.data
+#%%
+import torch
+x = torch.empty(5, 3)
+print(x)
+a = torch.empty([3, 4])
+print("a = ", a)
+
+b = torch.empty([2, 3])
+print("b = ", b)
+#%%
+x=torch.empty([3,4])
+print(x)
+
+y=x[:,1]
+x*=2
+print(y)
+print(x)
